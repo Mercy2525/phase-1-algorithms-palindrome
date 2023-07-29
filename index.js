14054e99eeeab641c27fc0663a8d756b9a92eee1
ASUS <you@example.com>
2023-07-29 12:16:26 +0300

    algorithm

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const MochaMulti = require("mocha-multi");
 
 
 function isPalindrome(word) {
-return word === word.split("").reverse().join("");
-
+if (word === word.split("").reverse().join("")){
+  return true
+} else{
+  return false
 }
-
-
-
-
+}
 
 /* 
   Add your pseudocode here
+
+  Reverse the string
+  if the reversed string is equal to the input string
+  return true
+  else return
+  false
 */
 
 /*
   Add written explanation of your solution here
+  When on inputs the word, mom, or abba, when the string is reversed, 
+  it should read the same.
+  If one inputs the word 'laptop' it should return false
+  since laptop reversed is 'potpal'
 */
 
 // You can run `node index.js` to view these console logs
